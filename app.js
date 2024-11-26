51ebe9b8f4fe9fb0ca2b8052fe35e86b444dc294
Matheus Chebel Vaz <matheuschebel@gmail.com>
2024-11-25 23:31:41 -0300

    Commit do projeto pós fim de aula

---
+++ app.js
@@ -0,0 +1,62 @@
+function sortear() {
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    let sorteados = [];
+    let numero;
+    let intervalo = ate - de;
+
+    
+    if (de > ate){
+        alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
+        reiniciar();
+        alterarStatusBotao();
+        return;
+    } else if (quantidade > intervalo){
+        alert('Quantidade inválida! O número digitado é superior a diferença dos números sorteados');
+        reiniciar();
+        alterarStatusBotao();
+        return;
+    }
+
+    for (let i  = 0; i < quantidade; i++){
+        numero = sortearNumAleatorio(de, ate);
+
+        while (sorteados.includes(numero)){
+            numero = sortearNumAleatorio(de, ate);
+        }
+    
+
+        sorteados.push(numero);
+    }
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`
+    alterarStatusBotao();
+}
+
+function sortearNumAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else{
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar(){
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotao();
+}
+
+
